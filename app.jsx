f0be8571cbc2ecf783bb4454fb8baea9a9a3469e
aishyuu <ivanmatias1331@gmail.com>
2023-01-07 21:49:19 -0800

    Added state switching functionality

--- app.jsx
+++ app.jsx
@@ -4,20 +4,38 @@ function App() {
     const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
     const [state, setState] = React.useState({
-        
+        completed: false,
+        response: "",
     })
 
-    function inputKey() {
-        
+    function inputKey(event) {
+        if(event.key === 'Enter' && state.completed === false) {
+            setState({
+                ...state,
+                completed: true
+            })
+        } else if (event.key === 'Enter' && state.completed === true) {
+            setState({
+                ...state,
+                completed: false
+            })
+        }
+    }
+
+    function updateResponse(event) {
+        setState({
+            ...state,
+            response: event.target.value
+        });
     }
 
     return(
         <div>
-            <div>
+            <div className={state.completed ? "finished" : ""}>
                 <h1>Swift Type</h1>
             </div>
             <div className="game_information">
-                <input autoFocus={true} onKeyDown />
+                <input type="text" autoFocus={true} onKeyDown={inputKey} onChange={updateResponse} value={state.response} />
             </div>
         </div>
     )
