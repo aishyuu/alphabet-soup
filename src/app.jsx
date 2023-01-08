c69ecfd72e1850fd50dc418508cdf940abd229bc
aishyuu <ivanmatias1331@gmail.com>
2023-01-08 05:59:53 -0800

    Finished Basic functionality

--- src/app.jsx
+++ src/app.jsx
@@ -3,14 +3,18 @@
 function App() {
     const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-    const countRef = React.useRef(null)
+    if(localStorage.getItem('best_time') == null){
+        localStorage.setItem('best_time', '')
+    }
+
+    let countRef = React.useRef(null)
 
     const [state, setState] = React.useState({
         completed: false,
         incorrect: false,
         response: "",
         current_index: 0,
-        time_running: false
+        best_time: localStorage.getItem('best_time')
     })
 
     const [elapsedTime, setElapsedTime] = React.useState(0)
@@ -37,18 +41,41 @@ function App() {
             });
         }
         if(state.current_index === 25) {
-            setState({
-                ...state,
-                completed: true
-            })
+            if(state.best_time === "" || state.best_time > elapsedTime) {
+                localStorage.setItem('best_time', elapsedTime.toString())
+                setState({
+                    ...state,
+                    completed: true,
+                    best_time: elapsedTime
+                })
+            } else {
+                setState({
+                    ...state,
+                    completed: true,
+                })
+            }
+            
             clearInterval(countRef.current)
         }
     }
 
     const run = () => {
         const startTime = Date.now() - elapsedTime;
+        
         countRef.current = setInterval(() => {
             setElapsedTime(Date.now() - startTime)
+        })
+    }
+
+    function stop() {
+        clearInterval(countRef.current)
+        console.log(typeof elapsedTime)
+        
+        setElapsedTime(0)
+        setState({
+            ...state,
+            current_index: 0,
+            completed: false
         })
     }
     
@@ -68,6 +95,28 @@ function App() {
             <div className="timer">
                 <h2>{("0" + Math.floor(elapsedTime/60000)).slice(-2)} : {("0" + Math.floor(elapsedTime/1000)).slice(-2)} : {("00" + elapsedTime%1000).slice(-3)}</h2>
             </div>
+            
+            {state.completed ?
+                <div>
+                    <button className="btn btn-secondary" onClick={stop}>Restart</button>
+                </div>
+                :
+                <div>
+                    <p></p>
+                </div>
+            }
+
+            {state.best_time === '' ?
+                <div>
+                    <p>Best Time - Complete a run</p>
+                </div>
+                :
+                <div>
+                    <p>Best Time - {("0" + Math.floor(state.best_time/60000)).slice(-2)} : {("0" + Math.floor(state.best_time/1000)).slice(-2)} : {("00" + state.best_time%1000).slice(-3)}</p>
+                </div>
+            }
+            
+
         </div>
     )
 }
